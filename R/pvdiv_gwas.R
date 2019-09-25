bc3fe3dffd34afce8db5c35824c455bb9ae0a57c
Alice MacQueen <alice.macqueen@gmail.com>
2019-09-25 14:35:57 -0500

    Add function to get lambda_GC for multiple PC #'s

--- R/pvdiv_gwas.R
+++ R/pvdiv_gwas.R
@@ -1,3 +1,114 @@
+#' Return lambda_GC for different numbers of PCs for GWAS on Panicum virgatum.
+#'
+#' @description Given a dataframe of phenotypes associated with PLANT_IDs and
+#'     output from a PCA to control for population structure, this function will
+#'     return a .csv file of the lambda_GC values for the GWAS upon inclusion
+#'     of different numbers of PCs. This allows the user to choose a number of
+#'     PCs that returns a lambda_GC close to 1, and thus ensure that they have
+#'     done adequate correction for population structure.
+#'
+#' @param df Dataframe of phenotypes where the first column is PLANT_ID and each
+#'     PLANT_ID occurs only once in the dataframe.
+#' @param type Character string. Type of univarate regression to run for GWAS.
+#'     Options are "linear" or "logistic".
+#' @param snp Genomic information to include for Panicum virgatum. Contact
+#'     tjuenger <at> utexas <dot> edu to obtain this information pre-publication.
+#' @param covar Covariance matrix to include in the regression. You
+#'     can generate these using \code{bigsnpr::snp_autoSVD()}.
+#' @param ncores Number of cores to use. Default is one.
+#' @param npcs Integer vector of principle components to use.
+#'     Defaults to c(0:10).
+#' @param saveoutput Logical. Should output be saved as a csv to the working directory?
+#'
+#' @import bigsnpr
+#' @import bigstatsr
+#' @importFrom dplyr mutate rename case_when
+#' @importFrom purrr as_vector
+#' @importFrom tibble as_tibble enframe
+#' @importFrom rlang .data
+#'
+#' @return A dataframe containing the lambda_GC values for each number of PCs
+#'     specified. This is also saved as a .csv file in the working directory.
+#'
+#' @export
+pvdiv_lambda_GC <- function(df, type = c("linear", "logistic"), snp,
+                       covar = NA, ncores = 1, npcs = c(0:10),
+                       saveoutput = FALSE){
+  if(colnames(df)[1] != "PLANT_ID"){
+    stop("First column of phenotype dataframe (df) must be 'PLANT_ID'.")
+    }
+  if(is.na(covar)){
+    stop(paste0("Need to specify covariance matrix (covar) and a vector of",
+                " PC #'s to test (npcs)."))
+  }
+  if(saveoutput == FALSE){
+    message("saveoutput is FALSE, so lambda_GC values won't be saved to a csv.")
+  }
+
+  G <- snp$genotypes
+  CHR <- snp$map$chromosome
+  POS <- snp$map$physical.pos
+
+  # Make the switchgrass chromosome names numeric, which bigsnpr requires.
+  CHRN <- enframe(CHR, name = NULL) %>%
+    dplyr::rename(CHR = .data$value) %>%
+    mutate(CHRN = case_when(.data$CHR == "Chr01K" ~ 1,
+                            .data$CHR == "Chr01N" ~ 2,
+                            .data$CHR == "Chr02K" ~ 3,
+                            .data$CHR == "Chr02N" ~ 4,
+                            .data$CHR == "Chr03K" ~ 5,
+                            .data$CHR == "Chr03N" ~ 6,
+                            .data$CHR == "Chr04K" ~ 7,
+                            .data$CHR == "Chr04N" ~ 8,
+                            .data$CHR == "Chr05K" ~ 9,
+                            .data$CHR == "Chr05N" ~ 10,
+                            .data$CHR == "Chr06K" ~ 11,
+                            .data$CHR == "Chr06N" ~ 12,
+                            .data$CHR == "Chr07K" ~ 13,
+                            .data$CHR == "Chr07N" ~ 14,
+                            .data$CHR == "Chr08K" ~ 15,
+                            .data$CHR == "Chr08N" ~ 16,
+                            .data$CHR == "Chr09K" ~ 17,
+                            .data$CHR == "Chr09N" ~ 18,
+                            TRUE ~ 19
+    ))
+
+  LambdaGC <- as_tibble(matrix(data =
+                                 c(npcs, rep(NA, (ncol(df) - 1)*length(npcs))),
+                               nrow = length(npcs), ncol = ncol(df)))
+  colnames(LambdaGC) <- colnames(df)
+  LambdaGC <- LambdaGC %>%
+    dplyr::rename("NumPCs" = PLANT_ID)
+
+  for(i in seq_along(names(df))[-1]){
+    y1 <- as_vector(df[!is.na(df[,i]), i])
+    ind_y <- which(!is.na(df[,i]))
+    for(k in npcs){
+      if(npcs[k] == 0){
+
+        gwaspc <- big_univLinReg(G, y.train = y1, ind.train = ind_y,
+                                 ncores = ncores)
+      } else {
+        ind_u <- matrix(covar$u[which(!is.na(df[,i])),1:npcs[k]], ncol = npcs[k])
+        gwaspc <- big_univLinReg(G, y.train = y1, covar.train = ind_u,
+                                 ind.train = ind_y, ncores = ncores)
+      }
+      gwas2 <- gwaspc[which(!is.na(gwaspc$score)),]
+      LambdaGC[i,k] <- bigsnpr:::getLambdaGC(gwas = gwas2)
+    }
+    if(saveoutput == TRUE){
+      write_csv(LambdaGC, path = paste0("Lambda_GC_", names(df)[i], ".rds"))
+      }
+  }
+  if(saveoutput == TRUE){
+    write_csv(LambdaGC, path = paste0("Lambda_GC_", names(df)[2], "_to_",
+                                      tail(names(df), n = 1), "_Phenotypes_",
+                                      npcs[1], "_to_", tail(npcs, n = 1),
+                                      "_PCs.rds"))
+  }
+  return(LambdaGC)
+}
+
 #' Wrapper for bigsnpr for GWAS on Panicum virgatum.
 #'
 #' @description Given a dataframe of phenotypes associated with PLANT_IDs, this
